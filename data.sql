c2b7eea1fbbbd81ce41ce3d19472916622d27e0a
kaixinrongzi <90364543+kaixinrongzi@users.noreply.github.com>
2022-11-29 21:48:47 -0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,30 @@
-INSERT INTO 
+-- Insert into Owners
+INSERT INTO Owner VALUES
+    (1, 'Corporation of Massey Hall', '647-888-8888'), 
+    (2, 'Roy Thomson Hall', '647-887-7878'),
+    (3, 'Maple Leaf Sports & Entertainment', '647-999-8080');
+
+-- Insert into Venues
+INSERT INTO Venue VALUES
+    (1, 'Massey Hall', 'Toronto', '178 Victoria Street'), 
+    (2, 'Roy Thomson Hall', 'Toronto', '60 Simcoe St'),
+    (3, 'Scotiabank Arena', 'Toronto', '40 Bay St');
+
+-- Insert into SectionInfo
+INSERT INTO SectionInfo VALUES
+    ('floor', 'Massey Hall'),
+    ('balcony', 'Massey Hall'),
+    ('main hall', 'Roy Thomson Hall'),
+    ('100', 'Scotiabank Arena'),
+    ('200', 'Scotiabank Arena'),
+    ('300', 'Scotiabank Arena');
+
+
+-- Insert into SeatInfo
+INSERT INTO SeatInfo VALUES
+    ('A1', 'floor', ),
+    ('balcony', 'Massey Hall'),
+    ('main hall', 'Roy Thomson Hall'),
+    ('100', 'Scotiabank Arena'),
+    ('200', 'Scotiabank Arena'),
+    ('300', 'Scotiabank Arena');
